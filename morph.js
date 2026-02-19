ec42705624439d69e47632addeb658202221cd14
Anton Medvedev <anton@medv.io>
2026-02-19 14:24:21 +0100

    Simplify DOM morphing logic by removing unnecessary cloning

--- morph.js
+++ morph.js
@@ -1,6 +1,6 @@
 export function morph(from, to) {
   if (from.nodeType !== to.nodeType) {
-    from.replaceWith(to.cloneNode(true))
+    from.replaceWith(to)
     return
   }
   if (from.nodeType === Node.TEXT_NODE || from.nodeType === Node.COMMENT_NODE) {
@@ -9,36 +9,36 @@ export function morph(from, to) {
   }
   if (from.nodeType === Node.ELEMENT_NODE) {
     if (from.tagName !== to.tagName) {
-      from.replaceWith(to.cloneNode(true))
+      from.replaceWith(to)
       return
     }
     syncAttributes(from, to)
     syncChildren(from, to)
     return
   }
-  from.replaceWith(to.cloneNode(true))
+  from.replaceWith(to)
 }
 
-function syncAttributes(fromEl, toEl) {
-  for (const {name} of Array.from(fromEl.attributes)) {
-    if (!toEl.hasAttribute(name)) fromEl.removeAttribute(name)
+function syncAttributes(from, to) {
+  for (const {name} of Array.from(from.attributes)) {
+    if (!to.hasAttribute(name)) from.removeAttribute(name)
   }
-  for (const {name, value} of Array.from(toEl.attributes)) {
-    if (fromEl.getAttribute(name) !== value) fromEl.setAttribute(name, value)
+  for (const {name, value} of Array.from(to.attributes)) {
+    if (from.getAttribute(name) !== value) from.setAttribute(name, value)
   }
 }
 
-function syncChildren(fromEl, toEl) {
-  const fromKids = Array.from(fromEl.childNodes)
-  const toKids = Array.from(toEl.childNodes)
+function syncChildren(from, to) {
+  const fromKids = Array.from(from.childNodes)
+  const toKids = Array.from(to.childNodes)
   const commonLen = Math.min(fromKids.length, toKids.length)
   for (let i = 0; i < commonLen; i++) {
     morph(fromKids[i], toKids[i])
   }
   for (let i = fromKids.length - 1; i >= toKids.length; i--) {
-    fromEl.removeChild(fromKids[i])
+    from.removeChild(fromKids[i])
   }
   for (let i = commonLen; i < toKids.length; i++) {
-    fromEl.appendChild(toKids[i].cloneNode(true))
+    from.appendChild(toKids[i])
   }
 }
